017695f0e069f75b35d21e04b0db2523297f6247
Anna Ritz <anna.m.ritz@gmail.com>
2014-12-30 14:21:34 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+## setup.py code modified from 
+## http://docs.cython.org/src/reference/compilation.html
+## to distribute cython modules.
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    name = "MBSV preprocessing app",
+    ext_modules = cythonize('src/insDelHmm.pyx'),  
+)
+
